b40fe568717b24600a0c19e6d0fe20eb710aa9d3
Nikita Kosarev <51489085+CaptainMorgan4392@users.noreply.github.com>
2020-10-03 09:18:17 +0700

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "LinkedListLib/LinkedList.h"
-#include "gtest/gtest.h"
-#include "gmock/gmock.h"
-
-int main(int argc, char** argv) {
-    testing::InitGoogleTest();
-
-    return RUN_ALL_TESTS();
-}

3eb1cc3d8263a8637fd8e3a64943c24a900dbcf2
Nikita Kosarev <51489085+CaptainMorgan4392@users.noreply.github.com>
2020-10-03 09:11:51 +0700

    Add files via upload

---
+++ lab0a/cmake-build-debug/CMakeFiles/lab0a.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/nikita/Desktop/lab0a/main.cpp" "/Users/nikita/Desktop/lab0a/cmake-build-debug/CMakeFiles/lab0a.dir/main.cpp.o"
+  "/Users/nikita/Desktop/lab0a/module1.cpp" "/Users/nikita/Desktop/lab0a/cmake-build-debug/CMakeFiles/lab0a.dir/module1.cpp.o"
+  "/Users/nikita/Desktop/lab0a/module2.cpp" "/Users/nikita/Desktop/lab0a/cmake-build-debug/CMakeFiles/lab0a.dir/module2.cpp.o"
+  "/Users/nikita/Desktop/lab0a/module3.cpp" "/Users/nikita/Desktop/lab0a/cmake-build-debug/CMakeFiles/lab0a.dir/module3.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

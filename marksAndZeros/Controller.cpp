24f9eea0b6562fe6c3208ed037c113b425504ed9
Nikita Kosarev <51489085+CaptainMorgan4392@users.noreply.github.com>
2020-12-05 22:17:51 +0700

    Update Controller.cpp

--- marksAndZeros/Controller.cpp
+++ marksAndZeros/Controller.cpp
@@ -1,14 +1,10 @@
 #include "Context.h"
 #include <iostream>
 
-bool Controller::updateRequest(std::pair<int, int> move, Model* model) {
+void Controller::updateRequest(std::pair<int, int> move, Model* model) {
     try {
         model -> setTo(move.first, move.second);
     } catch (ImpossibleMoveException &ex) {
         std::cerr << ex.what() << std::endl;
-        return false;
     }
-
-    return true;
 }
-

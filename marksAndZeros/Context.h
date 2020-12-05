d0db4bb25deac15f1d8c2b56f4462f22fa8da338
Nikita Kosarev <51489085+CaptainMorgan4392@users.noreply.github.com>
2020-12-05 22:04:52 +0700

    Update Context.h

--- marksAndZeros/Context.h
+++ marksAndZeros/Context.h
@@ -1,8 +1,76 @@
 #pragma once
-#include "Model.h"
-#include "View.h"
-#include "Controller.h"
+
+#include "ImpossibleMoveException.h"
 #include <iostream>
+#include <list>
+
+
+class Model;
+class Viewer;
+class Controller;
+
+enum States {
+    IN_PROGRESS,
+    X_WINS,
+    O_WINS,
+    DRAW
+};
+
+enum Signs {
+    MARKS,
+    ZEROS
+};
+
+class Model {
+    States state;
+
+    char field[3][3]{};
+
+    char curMove = 'x';
+
+    std::list <Viewer> observers;
+
+    void synchonize();
+
+    void endGameIfOver();
+
+public:
+    Model() {
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                field[i][j] = ' ';
+            }
+        }
+
+        state = IN_PROGRESS;
+    }
+
+    bool isFree(int first, int second);
+
+    States getState();
+
+    Signs getSign();
+
+    void setTo(int i, int j);
+
+    void setState(States state);
+
+    bool isGameEnded();
+
+    void addObserver(Viewer viewer);
+
+    char getFrom(int i, int j);
+};
+
+class Viewer {
+public:
+    void display(Model model);
+};
+
+class Controller {
+public:
+    bool updateRequest(std::pair <int, int> move, Model *model);
+};
 
 struct Context {
     Model model;

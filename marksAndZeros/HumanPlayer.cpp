d87e2e7ba03aefba044a2ccb77184e0dfce6bf37
Nikita Kosarev <51489085+CaptainMorgan4392@users.noreply.github.com>
2020-12-05 22:06:04 +0700

    Update HumanPlayer.cpp

--- marksAndZeros/HumanPlayer.cpp
+++ marksAndZeros/HumanPlayer.cpp
@@ -1,28 +1,11 @@
 #include "HumanPlayer.h"
-#include "ImpossibleMoveException.h"
 #include <iostream>
 
 std::pair <int, int> HumanPlayer::doMove() {
     int first, second;
 
-    bool moveDone = false;
-
-    while (!moveDone) {
-        std::cout << "Input your move in format: \"<numberLine> <numberColumn>\": ";
-        std::cin >> first >> second;
-        try {
-            if (first < 1 || first > 3 || second < 1 || second > 3) {
-                throw ImpossibleMoveException();
-            }
-        } catch (ImpossibleMoveException &ex) {
-            std::cerr << ex.what() << std::endl;
-            continue;
-        }
-
-        moveDone = true;
-    }
-
+    std::cin >> first >> second;
     std::pair <int, int> move {first - 1, second - 1};
+
     return move;
 }
-
